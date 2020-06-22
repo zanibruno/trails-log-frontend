cd9d8de2c94832c04f74333c2c237c042426aa20
zanibruno <corporate.zanin@gmail.com>
2020-06-22 18:19:02 +0100

    fixed typo

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMilddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import App from './App';
+import locationReducer from './reducers/locationReducer.js'
 
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancer(applyMiddleware(thunk))
+let store = createStore(locationReducer, composeEnhancer(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
 	<Provider store={store}>
 	<App />
-	</Provider>, 
-
+	</Provider>
+	, 
 	document.getElementById('root'));
 
 
