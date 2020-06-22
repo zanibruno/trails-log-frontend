3d1529f69476a3f9665da3b55f896b8592ada4a9
zanibruno <corporate.zanin@gmail.com>
2020-06-22 18:18:50 +0100

    class component app to render other components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import LocationsContainer from './containers/LocationsContainer.js'
 
 
 class App extends React.Component {
+
+
+
   render() {
   return(
     <div className='App'>
-      App
+      <LocationsContainer/>
     </div>
     );
 }
