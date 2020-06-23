b2ab4f8af2bbbcf31baf466a8061730d330abfe3
zanibruno <corporate.zanin@gmail.com>
2020-06-23 12:50:54 +0100

    Form for new location and call back functions to handleChange and Submit

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -1,12 +1,36 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addLocation} from '../actions/addLocation.js'
 
-export default class LocationInput extends React.Component {
+class LocationInput extends React.Component {
+
+	
+	state = {postcode: '', name: ''}
+
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.props.addLocation(this.state)
+	}
 
 	render() {
 		return(
 			<div>
-				Locations Input
+				<form onSubmit={this.handleSubmit}>
+				<label>Post Code:</label><br/>
+					<input type='text' placeholder='Post Code' value={this.state.postcode} name='postcode' onChange={this.handleChange}/><br/>
+					<label>Name:</label><br/>
+					<input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange}/><br/>
+					<input type='submit'/>
+				</form>
 			</div>
 		)
 	}
 }
+
+export default connect(null, { addLocation })(LocationInput)
