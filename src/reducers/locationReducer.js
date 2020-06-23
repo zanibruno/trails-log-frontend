d0a89aa599e7b134143de1055ca2e1581a23590a
zanibruno <corporate.zanin@gmail.com>
2020-06-23 12:51:34 +0100

    new action to create location and render without making new fetch requests.

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -5,6 +5,8 @@ export default function locationReducer(state = { locations: [] }, action ) {
 switch (action.type) {
 	case 'FETCH_LOCATIONS':
 		return {locations: action.payload}
+		case 'ADD_LOCATION':
+		return {...state, locations: [...state.locations, action.payload]}
 	default: 
 		return state
 } 
