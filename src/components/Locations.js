357b782cd1ad6fd82ad12a4ca0a1c0840f9f5808
zanibruno <corporate.zanin@gmail.com>
2020-06-25 18:59:11 +0100

    added links to location name, so i can access trails inside locations

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import {Route, Link} from 'react-router-dom'
 import Location from './Location.js'
 
 
 
 const Locations = (props) => {
 
-	return (
+	return(
 		<div>
 			{props.locations.map(location => 
-				<div key={location.id}><Location location={location}/></div>)}
+				<li key={location.id}>
+				<Link to={`/locations/${location.id}`}>{location.name}</Link>
+				</li>
+				)}
 		</div>
 		)
+
 }
 
 export default Locations
