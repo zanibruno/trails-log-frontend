2c0efdb34758aa9b187e9f3c4adc71f12a5b1f30
zanibruno <corporate.zanin@gmail.com>
2020-06-25 18:58:13 +0100

    getting location id from location.id instead of setting it from params.id -1

--- src/components/Location.js
+++ src/components/Location.js
@@ -7,17 +7,20 @@ import TrailsContainer from '../containers/TrailsContainer.js'
 
 const Location = (props) => {
 
-	let location = props.locations[props.match.params.id - 1]
-	// console.log(location)
-	console.log(props)
+	
+	// let location = props.locations[props.match.params.id - 1]
+		let location = props.locations.filter(location => location.id == props.match.params.id)[0]
+
+
+	
+
 	return(
 		<div>
-		<h2>
-		{location ? null : <Redirect to='/locations'/>}
-			{location ? location.name : null} - {location ? location.postcode : null}
-		
-		</h2>
-		<TrailsContainer location={location}/>
+			<h2>
+			{/*{location ? null : <Redirect to='/locations'/>}*/}
+				{location ? location.name : null} - {location ? location.postcode : null}
+			</h2>
+			<TrailsContainer location={location}/>
 		</div>
 	)
 
