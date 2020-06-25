c28e44f4067cadab087c0fc7165feb7e21ff76ec
zanibruno <corporate.zanin@gmail.com>
2020-06-25 19:01:39 +0100

    new case to add trail to backend and to the store

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,14 +1,22 @@
 
 
 export default function locationReducer(state = { locations: [] }, action ) {
-
 switch (action.type) {
 	case 'FETCH_LOCATIONS':
 		return {locations: action.payload}
 		case 'ADD_LOCATION':
 		return {...state, locations: [...state.locations, action.payload]}
-	default: 
+		case 'ADD_TRAIL':
+		let locations = state.locations.map(location => {
+			if(location.id === action.payload.id) {
+				return action.payload
+			} else {
+				return location
+			}
+		})
+		return {...state, locations: locations}
+		default: 
 		return state
-} 
+	} 
 
 }
