56169bb46ec19aefeda57339a23433599210fe1d
zanibruno <corporate.zanin@gmail.com>
2020-06-25 19:00:23 +0100

    iterating over trails array and rendering list

--- src/components/Trails.js
+++ src/components/Trails.js
@@ -3,11 +3,13 @@ import React from 'react'
 
 const Trails = (props) => {
 
-	return(
-		<div>
-			Trails component
-		</div>
-	)	
+	
+		return(
+			<div>
+				{props.trails && props.trails.map(trail => 
+					<li key={trail.id}>Trail Name: {trail.name} -- Trail Level: {trail.level} -- Trail Length: {trail.length}m -- Trail Description: {trail.description}</li>)}
+			</div>
+			)
 
 
 }
