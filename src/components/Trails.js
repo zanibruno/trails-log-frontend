a85f98213146bd2e9c8a52d5a65459f4f4705798
zanibruno <corporate.zanin@gmail.com>
2020-06-26 14:36:23 +0100

    importing files for basic styling and adding route to about

--- src/components/Trails.js
+++ src/components/Trails.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {deleteTrail} from '../actions/deleteTrail.js'
+import '../styles/TrailsContainer.css'
 
 
 const Trails = (props) => {
 
+
+	const handleDelete = (trail) => {
+		props.deleteTrail(trail.id, trail.location_id)
+	}
 	
 		return(
 			<div>
 				{props.trails && props.trails.map(trail => 
-					<li key={trail.id}>Trail Name: {trail.name} -- Trail Level: {trail.level} -- Trail Length: {trail.length}m -- Trail Description: {trail.description}</li>)}
+					<ul className='trails-list'>
+					<li key={trail.id} className='trail'>Trail Name: {trail.name} -- Trail Level: {trail.level} -- Trail Length: {trail.length}m -- Trail Description: {trail.description}
+					<button value='delete' onClick={() => handleDelete(trail)} className='delete-btn'>Delete</button>
+					</li>
+					</ul>)}
 			</div>
 			)
 
 
 }
 
-export default Trails
+export default connect(null, {deleteTrail})(Trails)
