52a8716a6a2415bf175e66ac3d19d4c0f2a79692
zanibruno <corporate.zanin@gmail.com>
2020-06-26 14:35:13 +0100

    importing files for basic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import {Router, Route} from 'react-router-dom'
 import LocationsContainer from './containers/LocationsContainer.js'
+import Home from './containers/Home.js'
+import About from './containers/About.js'
+import './styles/App.css'
 
 
 class App extends React.Component {
@@ -9,9 +13,11 @@ class App extends React.Component {
 
   render() {
   return(
+  	
     <div className='App'>
       <LocationsContainer/>
     </div>
+    
     );
 }
 }
